381e755c5ccdf216cc67d53600e1c48af6427c29
achelie <x771364026@gmail.com>
2023-05-31 09:52:31 +0800

    scss的自动导入

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,11 @@
 
 <template>
   <RouterView></RouterView>
+  <div class="test">测试自动导入</div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+  .test{
+    color: $helpColor;
+  }
+</style>
